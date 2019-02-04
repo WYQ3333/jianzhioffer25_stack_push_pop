7a602b1e3f54fb8912b366e9ac617961c7129ffc
浅忆梦微凉 <3538922590@qq.com>
2019-02-04 19:53:34 +0800

    牛客网代码未通过

--- 剑指offer25_栈的压入弹出序列/test.cpp
+++ 剑指offer25_栈的压入弹出序列/test.cpp
@@ -31,9 +31,9 @@ public:
 };
 
 void TestFunc(){
-	vector<int> array1 = { 1, 2, 3, 4, 5 };
+	vector<int> array1 = { 1 };
 	/*vector<int> array2 = { 4, 5, 3, 2, 1 };*/
-	vector<int> array2 = { 4, 3, 5, 1, 2 };
+	vector<int> array2 = {  1 };
 	Solution s;
 	cout << s.IsPopOrder(array1, array2) << endl;
 }
